30fe174e04470f558d295afcc26b9053a724656e
Riyal Guha <riiyalguha2002@gmail.com>
2024-12-08 00:45:41 +0530

    V2 modules rectified

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "resource_group" {
-  source = "./modules/c2-resource-group"
+  source = "modules/c2-resource-group"
 }
 
 module "virtual_network" {
-  source               = "./modules/c3-virtual-network"
+  source               = "modules/c3-virtual-network"
   resource_group_name  = module.resource_group.name
   resource_group_location = module.resource_group.location
 }
 
 module "virtual_machine" {
-  source = "./modules/c4-virtual-machine"
+  source = "modules/c4-virtual-machine"
   resource_group_name  = module.resource_group.name
   resource_group_location = module.resource_group.location
 }
